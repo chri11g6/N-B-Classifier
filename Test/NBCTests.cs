899ebfd9196ee91f40bf6b060c25e13eb3c3c5b5
Christian <15714004+chri11g6@users.noreply.github.com>
2022-09-28 22:01:42 +0200

    Add some Test

--- Test/NBCTests.cs
+++ Test/NBCTests.cs
@@ -10,10 +10,20 @@ public class NBCTests {
     // }
 
     [Test]
-    public void TotalOutput() {
+    public void Total_Output_Count() {
         const int totalOutputNumber = 2;
         Dictionary<string, double> outputs = nbc.Compute("Sunny,Cool,High,true");
 
         Assert.AreEqual(totalOutputNumber, outputs.Count);
+    }
+
+    [Test]
+    public void Get_Exception_For_Missing_Some_Input() {
+        Assert.Throws<Exception>(() => nbc.Compute("Cool,High,true"));
+    }
+
+    [Test]
+    public void Get_Exception_For_Wrong_Input() {
+        Assert.Throws<Exception>(() => nbc.Compute("Cool,High,true,Sunny"));
     }
 }
